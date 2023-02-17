f93dddf580a55514647c53bfca8862bcd57e866c
James Waltner <19035565+jwaltner@users.noreply.github.com>
2023-02-16 20:53:17 -0500

    arm pose comments

--- src/main/java/frc/robot/commands/ArmPoseControlCommand.java
+++ src/main/java/frc/robot/commands/ArmPoseControlCommand.java
@@ -35,6 +35,24 @@ public class ArmPoseControlCommand extends CommandBase {
   public void initialize() {}
 
   // Called every time the scheduler runs while the command is scheduled.
+  //
+  // You could possibly refine this, but transitioning between these 7 states sequentially in either 
+  // way should keep us from violating the rules.  Please review / validate that this is the case.
+  // Pose 0 - Pneumatic 0 - starting pose
+  // Pose 1 - Pneumatic 0 - pickup from human next to april command
+  // Pose 2 - Pneumatic 1 - same as pose 1, but transitioned pneumatics
+  // Pose 3 - Pneumatic 1 - top score pose
+  // Pose 4 - Pneumatic 0 - same as pose 3, but popped back
+  // Pose 5 - Pneumatic 0 - arm transitions to proper mid score pose, still in back position
+  // Pose 6 - Pneumatic 1 - pop forward to mid score position
+  // Pose 7 - Pneumatic 1 - lower arm to lower score / pickup item position
+  //
+  // Ramp ascent states.  Can go direct to this position from any one of 5,6,7 
+  // Pose x.1 - Pneumatic 1 - arm forward as we approach ramp, arm rotates down and pushes ramp down as we get closer to the ramp.
+  //
+  // Experiment to figure out a different set of states to ascend the ramp.
+  // * should we use april tag to know our distance and push down vs distance from april tag?
+  // * do we need an auxiliary arm specifically for ascending pointing in the oposite direction? (Yikes, more complexity... but maybe needed)
   @Override
   public void execute() {
     if(m_buttonNum == 5 || m_buttonNum == 6 || m_buttonNum == 0){

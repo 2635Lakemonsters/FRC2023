e43d5f1233189efc0c6fc8e5d0d2e4c1d1bad471
James Waltner <19035565+jwaltner@users.noreply.github.com>
2023-02-16 20:07:56 -0500

    arm motor subsystem comments

--- src/main/java/frc/robot/subsystems/ArmMotorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmMotorSubsystem.java
@@ -22,6 +22,14 @@ public class ArmMotorSubsystem extends SubsystemBase {
     // This method will be called once per scheduler run
   }
 
+  // feed forward for calculating power will require knowledge of state of the pneumatic controller.
+  // you will need to know the arm encoder 0 angle orientation in both pneumatic piston states.
+  // pick an orientation reletive to gravity to decide on what should be 0
+  // use that angle to calcualte a torque for feed forward so that the arm does not fall down.
+  // depending upon how you define what 0 is, your feed forward will be a different trigonometric function.
+  // you define the coordinate system so you can either make your life easy or difficult by your choice.
+  //
+  // will need feedback to put the arm in the actual desired state.
   public double calculatePower(double targetPose) { // target encoder pose
     double currentPose = 0; // Get current encoder pose 
     double delta = targetPose - currentPose;

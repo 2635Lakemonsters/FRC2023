50adb2514f3f8c746d97602b95d3e907314adbac
James Waltner <19035565+jwaltner@users.noreply.github.com>
2023-02-16 23:34:14 -0500

    notes summarizing chat on Thursday.

--- src/main/java/frc/robot/subsystems/ArmMotorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmMotorSubsystem.java
@@ -9,6 +9,30 @@ import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+// @Darren From our discussion on Thursday
+// Make a new subsystem to manage the overall arm.
+// * constructor parameters will include
+//   * ArmMotorSubsystem
+//   * ArmPneumaticSubsystem
+//   * ArmEncoderSensor or whatever
+// Calculate the appropriate feed forward to set in the ArmMotorSubsystem to take care of gravity.
+// Continuously feed that to ArmMotorSubsystem...  Note that in state transitions for pneumatics, we may have 
+// some bounce as the zero for the feed forward fluctuates, but that shouldn't be too bad, I hope... feed back 
+// should take care of it?
+//
+// the ArmMotorSubsystem should be responsible for applying whatever feed forward is needed.
+// the ArmSubsystem should be responsible for telling the ArmMotorSubsystem what feed forward it should be using
+//   since it knows about the state of the pneumatics.
+// the ArmSubsystem should also have the encoder so it knows how to calculate the feed forward from the combination of the 
+//   arm encoder and state of the pneumatic system
+// 
+// ====
+// Ramp Command
+// * pushing down the ramp might be posible with only the motor, will need finesse
+// * setting the arm in the right position to push down when pneumatics go forward might be good.
+//   * doing this will need a schedule of as you ascend the ramp, will need to move the arm.
+//   * potentially could get the initial position for this movement from distance of april tag on the oposite side of ramp 
+// .   if we are facing the april tags, Otherwise, don't know yet what to do...
 public class ArmMotorSubsystem extends SubsystemBase {
 
   private TalonFX armMotor = new TalonFX(0); //figure out the devie number

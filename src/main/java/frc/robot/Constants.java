4d39cf4218bf565a2106735735a299c715672dfb
Lakemonsters2635 <lakemonsters.software.team@gmail.com>
2023-10-07 17:59:13 -0700

    fixed the rotations

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,25 +13,25 @@ public final class Constants {
     public static final int DRIVETRAIN_FRONT_LEFT_ANGLE_MOTOR = 1; 
     public static final int DRIVETRAIN_FRONT_LEFT_ANGLE_ENCODER = 0; 
     public static final int DRIVETRAIN_FRONT_LEFT_DRIVE_MOTOR = 2; 
-    public static final double FRONT_LEFT_ANGLE_OFFSET_COMPETITION = Math.toRadians(-10 + 7.54); //3.01
+    public static final double FRONT_LEFT_ANGLE_OFFSET_COMPETITION = Math.toRadians(6.46); //3.01
 
     // FRONT RIGHT
     public static final int DRIVETRAIN_FRONT_RIGHT_ANGLE_MOTOR = 3; 
     public static final int DRIVETRAIN_FRONT_RIGHT_ANGLE_ENCODER = 1;
     public static final int DRIVETRAIN_FRONT_RIGHT_DRIVE_MOTOR = 4;
-    public static final double FRONT_RIGHT_ANGLE_OFFSET_COMPETITION = Math.toRadians(21 + 3.67); // 3.0775
+    public static final double FRONT_RIGHT_ANGLE_OFFSET_COMPETITION = Math.toRadians(12.67); // 3.0775
 
     // BACK LEFT
     public static final int DRIVETRAIN_BACK_LEFT_ANGLE_MOTOR = 5; 
     public static final int DRIVETRAIN_BACK_LEFT_ANGLE_ENCODER = 2;
     public static final int DRIVETRAIN_BACK_LEFT_DRIVE_MOTOR = 6; 
-    public static final double BACK_LEFT_ANGLE_OFFSET_COMPETITION = Math.toRadians(117.06); //2.9835
+    public static final double BACK_LEFT_ANGLE_OFFSET_COMPETITION = Math.toRadians(118.06); //2.9835
 
     // BACK RIGHT
     public static final int DRIVETRAIN_BACK_RIGHT_ANGLE_MOTOR = 7;
     public static final int DRIVETRAIN_BACK_RIGHT_ANGLE_ENCODER = 3;
     public static final int DRIVETRAIN_BACK_RIGHT_DRIVE_MOTOR = 8; 
-    public static final double BACK_RIGHT_ANGLE_OFFSET_COMPETITION = Math.toRadians(109.43); //3.0346
+    public static final double BACK_RIGHT_ANGLE_OFFSET_COMPETITION = Math.toRadians(-23.57); //3.0346
 
     public static final double kMaxModuleAngularSpeedRadiansPerSecond = 2 * Math.PI;
     public static final double kMaxModuleAngularAccelerationRadiansPerSecondSquared = 2 * Math.PI;
@@ -86,6 +86,8 @@ public final class Constants {
     public static final int SCORE_MID_RIGHT = 10;
     public static final int SCORE_BOTTOM_LEFT = 11;
     public static final int SCORE_BOTTOM_RIGHT = 12;
+
+    
 
     // hat constants 
     public static final int HAT_JOYSTICK_TRIM_POSITION = RIGHT_JOYSTICK_CHANNEL;
